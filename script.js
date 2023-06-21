d1969566303931a833018106a27b588519b242ad
Mamidimada Srinitha <98449441+MSrinitha@users.noreply.github.com>
2023-06-21 15:04:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,249 @@
+//Object Array to hold quotes, sources and citaitons
+var quotes = [
+	{
+	quote: "\"Detach yourself from the outcome and be free.\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"This too, shall pass.\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Happiness is the true measure of success, not wealth.\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"To begin your journey, you must start from where you are.\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Don't let the difficulties in life steal your happiness. Happiness is your true nature!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"It's always better to see half a glass full than half a glass empty.\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Fainthearted people give in to their fears. People who are courageous ignore their fears and take risks. Fear is the only thing that limits you!\"",
+	source: "Sador", 
+	citation: "Bhagavad Gita" 
+	},
+	{
+	quote:  "\"Winners are not born - they rise from the ashes of failures.  Those who endure failures and learn from them will succeed!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Possibilities are endless when no one limits you.\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Comparing yourself to yesterday will help you prepare for tomorrow.\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"One who is afraid to lose will never fall in love\"",
+	source: "Bhagavad Gita"
+	},
+	{
+	quote: "\"A small stream of water never becomes a reservoir without facing a dam. The struggles in life help you to discover who you are!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"The quality of your life will be determined by how disciplined you are.\"",
+	source: "Bhagavad Gita" 
+	
+	},
+	{
+	quote: "\"True Happiness in life comes from altruistic selfless acts!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Know that anything is possible, Even when it seems impossible!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Don't get too attached to the results, but be completely involved in the process\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"One who is not at peace can't bring peace to others!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Don't let success get to your head. Don't let failure discourage you. Both lead to disastrous results\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Fear of losing is the root cause of your worries. Stop worrying and start living!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Past Karma will decide your destiny but you can change your destiny through your present actions.\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+
+	quote: "\"Imagine if the caterpillar gave up, then it would never become a butterfly. Never give up even if you don't see an end to your suffering!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Life serves you the situations and how you respond to them will decide what price you pay. Your choices = Your karma\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"You don't get the fruits the day you plant the seed. Have patience; give some time. No one who does good work will ever come to a bed end!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Don't wait for great things to come your way, instead, do whatever comes to you in a great way.\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"The more distracted you are, The less successful you will be!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Happiness is not a goal to achieve but a journey you take!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"People who are grateful are always happy people!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Time heals all wounds, including a broken heart.\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"By comparing yourself with others, you lose sight of your own preparation!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Being in the present moment is the best place to be at any time!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"It is only through a peaceful mind that you can promote creativity, abundance, and peace.\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"You're made of your own beliefs!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"You can move mountains by moving small stones every day!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"The only way to accept reality is to let go of all your expectations!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"One day, you will be the best at what you do!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"The best way to deal with anxiety is to act\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Trust in yourself that you are as special as you are\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Confidence comes from overcoming challenges in life!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Today, make a better version of yourself than yesterday!\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Trust is the foundation for any relationship\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"Look at life on the bright side, it's all about perspective\"",
+	source: "Bhagavad Gita" 
+	},
+	{
+	quote: "\"For the doubting soul, there is no happiness in this life and the life after\"",
+	source: "Bhagavad Gita" 
+	},
+    {
+    quote: "\"You have the right to perform your prescribed duty, but you are not entitled to the fruits of your actions.\"",
+    source: "Bhagavad Gita" 
+	},
+    {
+    quote: "\"Change is the law of the universe. You can be a millionaire, or a pauper in an instant.\"",
+    source: "Bhagavad Gita" 
+	},
+    {
+    quote: "\"The soul is neither born, and nor does it die.\"",
+    source: "Bhagavad Gita" 
+	},
+    {
+    quote: "\"There is neither this world, nor the world beyond. Nor happiness for the one who doubts.\"",
+    source: "Bhagavad Gita" 
+	},
+    {
+    quote: "\"A person can rise through the efforts of his own mind; mind is the friend and the enemy of the self.\"",
+    source: "Bhagavad Gita" 
+	},
+    {
+    quote: "\"A man's own self is his friend. A man's own self is his foe.\"",
+    source: "Bhagavad Gita" 
+	},
+    {
+    quote: "\"When meditation is mastered, the mind is unwavering like the flame of a lamp in a windless place.\"",
+    source: "Bhagavad Gita" 
+	},
+    {
+    quote: "\"The soul can never be cut into pieces by any weapon, nor can he be burned by fire, nor moistened by water, nor withered by the wind.\"",
+    source: "Bhagavad Gita" 
+	},
+    {
+    quote: "\"The soul can never be cut to pieces by any weapon, nor burned by fire, nor moistened by water, nor withered by the wind.\"",
+    source: "Bhagavad Gita" 
+	}
+];
+
+//Function to randomly select a quote value and return a random quote object from the quotes array
+function getRandomQuote () {
+	var randomNumber = Math.floor(Math.random() * (quotes.length));
+	var randomQuote = quotes[randomNumber];
+	return randomQuote;
+}	
+
+//Function to select random rgb color value
+function getRandomColor () {
+	var red = Math.floor(Math.random() * 256 );
+	var green = Math.floor(Math.random() * 256 );
+	var blue = Math.floor(Math.random() * 256 );
+	 var randomColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+	return randomColor;
+}
+
+//Function to call the getRandomQuote function and stores the returned quote object in a variable
+//Constructs a string containing the different properties of the quote object 
+function printQuote () {
+		var quotes = getRandomQuote ();
+		var quoteContainer = document.getElementById("quote-box");
+		var quoteString = `<p class="quote">${quotes.quote}</p><p class="source">${quotes.source}`;
+				if (quotes.citation) {quoteString += `<span class="citation">${quotes.citation}</span></p>`}
+				
+				else {quoteString += '</p>'};
+					quoteContainer.innerHTML = quoteString;
+
+					//assigns random color value to document background color 
+					document.body.style.backgroundColor = getRandomColor ();
+}
+
+//Quote automatically refreshes every 20 seconds
+window.setInterval(function(){
+printQuote ();
+}, 20000);
+
+//Event listener on LoadQuote button to generate new quote		
+document.getElementById("loadQuote").addEventListener("click", printQuote, false);
